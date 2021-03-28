2bfd86abf29cef965e4cd7b8506b74c6a7726635
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-03-28 17:43:00 -0600

    inserting roles data seed

--- seeds.sql
+++ seeds.sql
@@ -4,3 +4,12 @@ VALUES ("Sales"),
  ("Customer Service"),
  ("Engineering"),
  ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer",150000.00,5),
+("Human Resource Manager",65000.00,2),
+("Customer Care Agent",45000.00,3),
+("Sales Representative",60000.00,1),
+("Sales Team Lead",85000.00,1),
+("Junior Engineer",65000.00,4),
+("Senior Engineer",80000.00,4);
