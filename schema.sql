bf2d826f1933b98a7fbd6758198a5edfc64e0fb4
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-03-28 16:41:57 -0600

    employees table added

--- schema.sql
+++ schema.sql
@@ -2,3 +2,12 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
+
+CREATE TABLE employees(
+id INT AUTO_INCREMENT NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL, 
+role_id INT NOT NULL,
+manager_id INT
+PRIMARY KEY (id)
+);
