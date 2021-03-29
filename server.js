2aa1eae99ed033a3a2ccb7823abc7233b9381d37
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-03-28 18:19:07 -0600

    inquirer questions addedd

--- server.js
+++ server.js
@@ -8,6 +8,42 @@ const connection = mysql.createConnection({
     user: 'root',
     password: 'root',
     database: 'employees_db',
-  });
-  
-  
+});
+
+const start = () => {
+    inquirer.prompt({
+        type: "list",
+        name: "method",
+        message: "What would you like to do?",
+        choices: ["Add Employee", "Add a Department", "Add a Role", "Update Employee Roles", "View All Employees", "View All Departments", "View All Roles", "Exit"]
+    })
+        .then((answer) => {
+            switch (answer.method) {
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Add a Department":
+                    addDepartment();
+                    break;
+                case "Add a Role":
+                    addRole();
+                    break;
+                case "Update Employee Roles":
+                    updateEmployee();
+                    break;
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+                case "View All Departments":
+                    viewAllDepartments();
+                    break;
+                case "View All Roles":
+                    viewAllRoles();
+                    break;
+                default:
+                    connection.end();
+
+
+            }
+        });
+};
