e6105609fec1647698884682ec2a24f3b1860b38
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-03-28 18:04:23 -0600

    inserting employee data seed

--- seeds.sql
+++ seeds.sql
@@ -5,7 +5,7 @@ VALUES ("Sales"),
  ("Engineering"),
  ("Legal");
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES ("Lawyer",150000.00,5),
 ("Human Resource Manager",65000.00,2),
 ("Customer Care Agent",45000.00,3),
@@ -13,3 +13,12 @@ VALUES ("Lawyer",150000.00,5),
 ("Sales Team Lead",85000.00,1),
 ("Junior Engineer",65000.00,4),
 ("Senior Engineer",80000.00,4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Michael", "Scott", 5, 1), 
+("Jim", "Halpert", 4, 1),
+("Pam", "Beesly", 3,2),
+("Andy","Bernard",5,4),
+("Phyllis", "Vance",2,3),
+("Dwight", "Shrute",7,1),
+("Angela", "Martin",6,6);
