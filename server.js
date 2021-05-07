b9d1d4e98d27ca421140a72e4baf8cb3e744f206
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-05-07 13:03:32 -0600

    reworking file structure

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const mysql = require('mysql');
+//const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'employees_db',
-});
+//moving connection to a a connection.js file
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: 'root',
+//     database: 'employees_db',
+// });
 
 const start = () => {
     inquirer.prompt({
